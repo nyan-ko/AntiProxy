71a491502825a5ea4951175b000560042f9491d4
nyan-ko <dumbstupidcatboy@gmail.com>
2020-12-09 19:00:04 -0500

    whitelists

--- AntiProxy.cs
+++ AntiProxy.cs
@@ -1,9 +1,14 @@
 ﻿using AntiProxy.Enums;
 using System.Diagnostics;
 using System.IO;
+using System.Collections.Generic;
+using System;
 using Terraria;
+using System.Net;
 using Terraria.Localization;
 using TerrariaApi.Server;
+using System.Linq;
+using System.Globalization;
 using TShockAPI;
 
 namespace AntiProxy
@@ -17,6 +22,7 @@ namespace AntiProxy
         public override string Author => "nyan";
 
         public Verifier Verifier { get; private set; }
+        public WhitelistDatabase Database { get; private set; }
 
         public AntiProxy(Main game) : base(game)
         {
@@ -38,7 +44,10 @@ namespace AntiProxy
                 return;
             }
 
+            Commands.ChatCommands.Add(new Command("antiproxy.admin", APCommand, "apwhitelist", "apwl"));
+
             Verifier = new Verifier(email);
+            Database = new WhitelistDatabase(TShock.DB);
 
             ServerApi.Hooks.ServerJoin.Register(this, OnJoin);
         }
@@ -52,14 +61,175 @@ namespace AntiProxy
 
             if (risk != RiskType.Low)
             {
+                if (Database.IsWhitelisted(ip))
+                {
+                    TShock.Log.ConsoleInfo($"Whitelisted IP {ip} allowed through.");
+                    TShock.Log.Write($"Whitelisted IP {ip} allowed through.", TraceLevel.Info);
+                    return;
+                }
                 TShock.Log.ConsoleInfo($"Risk: {risk} for IP: {ip}");
                 TShock.Log.Write($"Risk: {risk} for IP: {ip}", TraceLevel.Info);
 
                 client.PendingTermination = true;
                 //client.PendingTerminationApproved = true;
-                NetMessage.SendData((int)PacketTypes.Disconnect, args.Who, -1, NetworkText.FromLiteral("Disconnected: possible proxy. Contact server admins if you have questions."));
+                NetMessage.SendData((int)PacketTypes.Disconnect, args.Who, -1, NetworkText.FromLiteral($"Disconnected: possible proxy. Contact server admins if you have questions; your code is {EncodeIP(ip)}."));
                 args.Handled = true;
             }
+        }
+
+        private void APCommand(CommandArgs args)
+        {
+            var p = args.Player;
+
+            Action<string, List<string>> info = (string help, List<string> lines) =>
+            {
+                p.SendInfoMessage(help);
+                p.SendInfoMessage(string.Join("\n", lines));
+            };
+
+            string sub = args.Parameters.Count == 0 ? "help" : args.Parameters[0].ToLower();
+            switch (sub)
+            {
+                case "help":
+                    var cmds = new List<string>()
+                    {
+                        "add - Adds an ip, encoded or normal, to the antiproxy whitelist.",
+                        "del - Deletes an ip from the whitelist.",
+                        "list - Lists current whitelisted ips."
+                    };
+
+                    info("Command for handling anti-proxy whitelists.", cmds);
+                    break;
+                case "add":
+                {
+                    var help = new List<string>()
+                    {
+                        "Usage: add -e/-n",
+                        "-e - Adds an encoded IP (e.g. A3-4D-8C-14)",
+                        "-n - Adds a normal IP (e.g. 127.0.0.1)"
+                    };
+
+                    if (args.Parameters.Count < 3)
+                    {
+                        info("Subcommand for whitelisting an ip.", help);
+                    }
+                    else
+                    {
+                        string ip = args.Parameters[2];
+
+                        if (args.Parameters[1] == "-e")
+                        {
+                            ip = DecodeIP(ip)?.ToString();
+                        }
+                        else if (args.Parameters[1] != "-n")
+                        {
+                            info("Subcommand for whitelisting an ip.", help);
+                        }
+
+                        if (!IPAddress.TryParse(ip, out IPAddress unused))
+                        {
+                            p.SendErrorMessage($"Could not parse {ip} as an ip.");
+                        }
+                        if (!Database.TryAddWhitelist(ip))
+                        {
+                            p.SendErrorMessage("Could not complete database query.");
+                        }
+                        else
+                        {
+                            p.SendSuccessMessage($"Successfully whitelisted {ip}.");
+                        }
+                    }
+                }
+                    break;
+                case "del":
+                {
+                    var help = new List<string>()
+                    {
+                        "Usage: del -e/-n",
+                        "-e - Removes an encoded IP (e.g. A3-4D-8C-14)",
+                        "-n - Removes a normal IP (e.g. 127.0.0.1)"
+                    };
+
+                    if (args.Parameters.Count < 3)
+                    {
+                        info("Subcommand for removing an ip from the whitelist.", help);
+                    }
+                    else
+                    {
+                        string ip = args.Parameters[2];
+
+                        if (args.Parameters[1] == "-e")
+                        {
+                            ip = DecodeIP(ip)?.ToString();
+                        }
+                        else if (args.Parameters[1] != "-n")
+                        {
+                            info("Subcommand for removing an ip from the whitelist..", help);
+                        }
+
+                        if (!IPAddress.TryParse(ip, out IPAddress unused))
+                        {
+                            p.SendErrorMessage($"Could not parse {ip} as an ip.");
+                        }
+                        if (!Database.TryRemoveWhitelist(ip))
+                        {
+                            p.SendErrorMessage("Could not complete database query.");
+                        }
+                        else
+                        {
+                            p.SendSuccessMessage($"Successfully removed {ip} from the whitelist.");
+                        }
+                    }
+                }
+                    break;
+                case "list":
+                {
+                    if (!PaginationTools.TryParsePageNumber(args.Parameters, 1, p, out int pgN))
+                    {
+                        return;
+                    }
+
+                    var ips = Database.GetAllWhitelists();
+
+                    PaginationTools.SendPage(p, pgN, ips.ToList(),
+                        new PaginationTools.Settings
+                        {
+                            HeaderFormat = "IP Whitelist ({0}/{1})",
+                            FooterFormat = "Type /apwhitelist list {0} for more.",
+                            NothingToDisplayString = "There are currently no whitelisted IPs."
+                        });
+                }
+                    break;
+            }
+        }
+
+        public static string EncodeIP(string ip)
+        {
+            if (!IPAddress.TryParse(ip, out IPAddress adr))
+            {
+                return "";
+            }
+
+            byte[] componentBytes = IPAddress.Parse(ip).GetAddressBytes();
+
+            return BitConverter.ToString(componentBytes);
+        }
+
+        public static IPAddress DecodeIP(string encodedIp)
+        {
+            string[] encodedComp = encodedIp.Split('-');
+            byte[] components = new byte[encodedComp.Length];
+
+            for (int i = 0; i < encodedComp.Length; i++)
+            {
+                if (!byte.TryParse(encodedComp[i], NumberStyles.AllowHexSpecifier, null, out byte norm))
+                {
+                    return null;
+                }
+                components[i] = norm;
+            }
+
+            return new IPAddress(components);
         }
     }
 }
---
+++ WhitelistDatabase.cs
@@ -0,0 +1,102 @@
+﻿using System.Data;
+using System.Data.Sql;
+using TShockAPI;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+using System;
+using TShockAPI.DB;
+using System.Collections.Generic;
+
+namespace AntiProxy
+{
+    public class WhitelistDatabase
+    {
+        private IDbConnection _db;
+
+        public WhitelistDatabase(IDbConnection db)
+        {
+            _db = db;
+
+            var table = new SqlTable("AntiProxyWhitelists",
+                new SqlColumn("IP", MySqlDbType.String, 16) { Primary = true });
+
+            var ctor = new SqlTableCreator(db,
+                db.GetSqlType() == SqlType.Mysql
+                ? new MysqlQueryCreator()
+                : (IQueryBuilder)new SqliteQueryCreator());
+
+            ctor.EnsureTableStructure(table);
+        }
+
+        public bool IsWhitelisted(string ip)
+        {
+            using (var reader = _db.QueryReader("SELECT * FROM AntiProxyWhitelists WHERE IP=@0", ip))
+            {
+                return reader.Read();
+            }
+        }
+
+        public bool TryAddWhitelist(string ip)
+        {
+            try
+            {
+                if (IsWhitelisted(ip))
+                {
+                    return true;
+                }
+                else
+                {
+                    return _db.Query("INSERT INTO AntiProxyWhitelists (IP) VALUES (@0);", ip) == 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                TShock.Log.ConsoleError(ex.ToString());
+                return false;
+            }
+        }
+
+        public bool TryRemoveWhitelist(string ip)
+        {
+            try
+            {
+                if (!IsWhitelisted(ip))
+                {
+                    return true;
+                }
+                else
+                {
+                    return _db.Query("DELETE FROM AntiProxyWhitelist WHERE IP=@0", ip) == 1;
+                }
+            }
+            catch(Exception ex)
+            {
+                TShock.Log.ConsoleError(ex.ToString());
+                return false;
+            }
+        }
+
+        public IEnumerable<string> GetAllWhitelists()
+        {
+            List<string> ips = new List<string>();
+
+            try
+            {
+                using (var reader = _db.QueryReader("SELECT * FROM AntiProxyWhitelists"))
+                {
+                    while (reader.Read())
+                    {
+                        ips.Add(reader.Get<string>("IP"));
+                    }
+
+                    return ips;
+                }
+            }
+            catch(Exception ex)
+            {
+                TShock.Log.ConsoleError(ex.ToString());
+                return null;
+            }
+        }
+    }
+}
